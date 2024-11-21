f6a942e2a211d7e48467a0fd6a11b3feaffee7c7
dperez-a <dpereza0503@gmail.com>
2024-11-21 12:48:58 +0100

    problems solved

--- src/main.c
+++ src/main.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dani_mm__ <dani_mm__@student.42.fr>        +#+  +:+       +#+        */
+/*   By: dperez-a <dperez-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 11:25:04 by dani_mm__         #+#    #+#             */
-/*   Updated: 2024/03/25 13:15:51 by dani_mm__        ###   ########.fr       */
+/*   Updated: 2024/11/21 12:47:14 by dperez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-//! En esta función main no paso los argumentos dados por split
-/*int	main(int argc, char **argv)
+
+int	main(int argc, char **argv)
 {
 	t_node	*stack_a;
 	t_node	*stack_b;
@@ -32,59 +32,20 @@
 	}
 	ft_lstclear_ps(&stack_a);
 	ft_lstclear_ps(&stack_b);
-}*/
+}
 
-/*int	main(int argc, char **argv)
-{
-    t_node	*stack_a;
-    t_node	*stack_b;
-    int i = 1;
-
-    if (argc < 2)
-        exit (1);
-    stack_a = NULL;
-    stack_b = NULL;
-    while (i < argc)
-    {
-        char **numbers = split_string(argv[i]);
-        int j = 0;
-        while (numbers[j] != NULL)
-        {
-            t_node *new_stack = ft_nb_to_stack(2, (char *[]){argv[0], numbers[j], NULL});
-            if (!new_stack)
-                ft_exit_error(&stack_a);
-            add_to_stack(&stack_a, new_stack->nb);
-            j++;
-        }
-        free(numbers);
-        i++;
-    }
-    if (a_is_sorted(stack_a) == 1)
-    {
-        if (argc < 7)
-            ft_sort_few(&stack_a, &stack_b, argc);
-        else
-            radix_sort(&stack_a, &stack_b, argc);
-    }
-    ft_lstclear_ps(&stack_a);
-    ft_lstclear_ps(&stack_b);
-}*/
-
-// Parte 1: Manejo de argumentos de línea de comandos
 void handle_command_line_arguments(int argc)
 {
     if (argc < 2)
         exit(1);
 }
 
-// Parte 2: Inicialización de las pilas
 void initialize_stacks(t_node **stack_a, t_node **stack_b)
 {
     *stack_a = NULL;
     *stack_b = NULL;
 }
 
-// Parte 3: Procesamiento de los argumentos
 void process_arguments(int argc, char **argv, t_node **stack_a)
 {
     int i = 1;
@@ -104,7 +65,6 @@ void process_arguments(int argc, char **argv, t_node **stack_a)
     }
 }
 
-// Parte 4: Ordenamiento y limpieza
 void sort_and_cleanup(t_node **stack_a, t_node **stack_b, int argc)
 {
     if (a_is_sorted(*stack_a) == 1)
@@ -116,25 +76,4 @@ void sort_and_cleanup(t_node **stack_a, t_node **stack_b, int argc)
     }
     ft_lstclear_ps(stack_a);
     ft_lstclear_ps(stack_b);
-}
-
-// Función main que llama a las partes anteriores
-int	main(int argc, char **argv)
-{
-    t_node *stack_a;
-    t_node *stack_b;
-
-    // Parte 1: Manejo de argumentos de línea de comandos
-    handle_command_line_arguments(argc);
-
-    // Parte 2: Inicialización de las pilas
-    initialize_stacks(&stack_a, &stack_b);
-
-    // Parte 3: Procesamiento de los argumentos
-    process_arguments(argc, argv, &stack_a);
-
-    // Parte 4: Ordenamiento y limpieza
-    sort_and_cleanup(&stack_a, &stack_b, argc);
-
-    return 0;
 }
--- src/parse.c
+++ src/parse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dani_mm__ <dani_mm__@student.42.fr>        +#+  +:+       +#+        */
+/*   By: dperez-a <dperez-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 11:25:11 by dani_mm__         #+#    #+#             */
-/*   Updated: 2024/03/20 12:09:11 by dani_mm__        ###   ########.fr       */
+/*   Updated: 2024/11/21 12:46:15 by dperez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,25 +32,30 @@ void	add_to_stack(t_node **stack, int nb)
 	ft_lstadd_back_ps(stack, new);
 }
 
-int	ft_is_dupplicate(t_node **stack)
+int ft_is_duplicate(t_node **stack)
 {
-	t_node	*current;
-	t_node	*check;
+    t_node *current;
+    t_node *check;
 
-	current = (*stack);
-	while (current != NULL)
-	{
-		check = current->next;
-		while (check != NULL)
-		{
-			if (current->nb == check->nb)
-				return (1);
-			check = check->next;
-		}
-		current = current->next;
-	}
-	return (0);
+    if (stack == NULL || *stack == NULL)
+        ft_exit_error(stack);
+
+    current = (*stack);
+    while (current != NULL)
+    {
+        check = current->next;
+        while (check != NULL)
+        {
+            if (current->nb == check->nb)
+                return 1;
+            check = check->next;
+        }
+        current = current->next;
+    }
+    return 0;
 }
+
+
 
 int	ft_check_argv_is_digit(char *str)
 {
@@ -61,7 +66,7 @@ int	ft_check_argv_is_digit(char *str)
         if (!(*str >= '0' && *str <= '9') && *str != '-')
             return (1);
         if (*str == '-' && *(str + 1) == '-')
-            return (1); // error: signo '-' repetido
+            return (1);
         str++;
     }
     return (0);
@@ -77,6 +82,8 @@ t_node	*ft_nb_to_stack(int argc, char **argv)
 	stack = NULL;
 	while (i <= argc - 1)
 	{
+		if (argv[i][0] == '\0')
+			ft_exit_error(&stack);
 		if (ft_check_argv_is_digit(argv[i]) == 1)
 			ft_exit_error(&stack);
 		else
@@ -86,7 +93,7 @@ t_node	*ft_nb_to_stack(int argc, char **argv)
 		}
 		i++;
 	}
-	if (ft_is_dupplicate(&stack) == 1)
+	if (ft_is_duplicate(&stack) == 1)
 		ft_exit_error(&stack);
 	put_index_stack(&stack);
 	return (stack);
